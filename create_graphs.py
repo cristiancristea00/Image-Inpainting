7d8383e7316e33321d2942cea4ede0fb2b12cb11
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-04-28 23:02:34 +0300

    Update models

--- create_graphs.py
+++ create_graphs.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 import json
 import multiprocessing as mp
-import time
-import traceback
 from pathlib import Path
 
 from colorama import Fore, Style
 
 from graphs_generator import GraphsGenerator
+from utils import time_running_cpu
 
 
 def create_graph(path: Path) -> None:
@@ -21,40 +20,13 @@ def create_graph(path: Path) -> None:
     graphs_generator.create_all_graphs()
 
 
-def run() -> None:
+def main() -> None:
     models_path = Path('models')
 
-    with mp.Pool() as processes_pool:
-        processes_pool.map(create_graph, models_path.iterdir())
-
-
-def main() -> None:
-    print(Fore.MAGENTA + 'Starting script...' + Style.RESET_ALL, flush=True)
-
-    start_time = time.perf_counter()
-    try:
-        mp.set_start_method('fork')
-        run()
-
-    except KeyboardInterrupt:
-
-        print(Fore.RED + '\nScript interrupted by the user!' + Style.RESET_ALL, flush=True)
-
-    except Exception:
-
-        print(Fore.RED, flush=True)
-        print('Script failed with the following error:', flush=True)
-        traceback.print_exc()
-        print(Style.RESET_ALL, flush=True)
-
-    end_time = time.perf_counter()
-
-    elapsed = end_time - start_time
-    elapsed_time = time.strftime('%H:%M:%S', time.gmtime(elapsed))
-
-    print(Fore.YELLOW + F'Total time: {elapsed_time}' + Style.RESET_ALL, flush=True)
-    print(Fore.MAGENTA + 'Everything done!' + Style.RESET_ALL, flush=True)
+    for model_folder in models_path.iterdir():
+        with mp.Pool() as processes_pool:
+            processes_pool.map(create_graph, model_folder.iterdir())
 
 
 if __name__ == '__main__':
-    main()
+    time_running_cpu(main)
